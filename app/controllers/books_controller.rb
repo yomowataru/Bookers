39524a1c6e6877399b8761724390ebe9c817e360
EC2 Default User <ec2-user@ip-172-31-12-15.ap-northeast-1.compute.internal>
2023-05-06 13:49:34 +0000

    [Add] 3-12 課題2 CRUD実装(新規登録、保存機能まで)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,22 @@
 class BooksController < ApplicationController
-  def new
+  def create
+    book = Book.new(book_params) 
+    book.save 
+    redirect_to '/books/:id' 
   end
 
   def index
+    @book = Book.new
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body) 
   end
 end
