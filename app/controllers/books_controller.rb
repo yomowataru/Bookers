8288fe1cf63dd3971b5299c65bd52d615d638201
EC2 Default User <ec2-user@ip-172-31-12-15.ap-northeast-1.compute.internal>
2023-05-07 07:28:04 +0000

    [Add] 3-12 課題2 バリデーション実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,13 @@
 class BooksController < ApplicationController
   def create
-    book = Book.new(book_params) 
-    book.save 
-    flash[:notice] = "Book was successfully created."
-    redirect_to book_path(book.id)  
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)  
+    else
+      render :index
+    end
   end
 
   def index
@@ -20,10 +24,13 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book.id) 
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else 
+      render :edit 
+    end
   end
   
   def destroy 
@@ -34,7 +41,7 @@ class BooksController < ApplicationController
   end
   
   private
-  def book_params
+  def book_params 
     params.require(:book).permit(:title, :body) 
   end
 end
